33722fea9ecb157963529145f42ae4006a016639
David Bouscarle <d.bouscarle@gmail.com>
2021-07-06 17:43:36 +0200

    'Implement the BootStrap Template, must clean dependencies not use'

--- assets/app.js
+++ assets/app.js
@@ -7,9 +7,23 @@
 
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
+import './styles/css/LineIcons.2.0.css';
+import './styles/css/animate.css';
+import './styles/css/tiny-slider.css';
+import './styles/css/glightbox.min.css';
+import './styles/css/main.css';
 
 // You can specify which plugins you need
 import { Tooltip, Toast, Popover } from "bootstrap";
+// import './js/count-up.min';
+// import './js/wow.min';
+import './js/tiny-slider';
+import './js/glightbox.min';
+import './js/imagesloaded.min';
+// import './js/jquery-counterup.min';
+import './js/isotope.min';
+import './js/main';
 
 // start the Stimulus application
 import './bootstrap';
+
