04f1d2e46d185440ee96d7212755251f2c4da022
David Bouscarle <d.bouscarle@gmail.com>
2021-08-03 09:20:10 +0200

    NavBar finished

--- assets/js/navBar.js
+++ assets/js/navBar.js
@@ -9,47 +9,53 @@ const mobileMenu = document.getElementById("mobileMenu");
 
 // HANDLE DROPDOWN MENU
 
-const add180Degree = (degree) => {
-    let newDeg = degree - 180;
-    return newDeg;
-};
-
 linkDropdown.addEventListener("click", () => {
-    if (navbarDropdown.classList.contains("hide__base__dropdown")) {
-        navbarDropdown.classList.remove("hide__base__dropdown");
-        navbarDropdown.classList.add("display__dropdown");
-        arrowDropdown.style.transform = `rotate(${add180Degree(0)}deg)`;
+    if (navbarDropdown.classList.contains("hide__base")) {
+        navbarDropdown.classList.remove("hide__base");
+        navbarDropdown.classList.add("display__visible");
+        arrowDropdown.style.transform = "rotate(-180deg)";
         arrowDropdown.style.transition = "250ms";
-    } else if (navbarDropdown.classList.contains("hide__dropdown")) {
-        navbarDropdown.classList.remove("hide__dropdown");
-        navbarDropdown.classList.add("display__dropdown");
-        arrowDropdown.style.transform = `rotate(${add180Degree(0)}deg)`;
+    } else if (navbarDropdown.classList.contains("hide__visible")) {
+        navbarDropdown.classList.remove("hide__visible");
+        navbarDropdown.classList.add("display__visible");
+        arrowDropdown.style.transform = "rotate(-180deg)";
     } else {
-        navbarDropdown.classList.remove("display__dropdown");
-        navbarDropdown.classList.add("hide__dropdown");
-        arrowDropdown.style.transform = `rotate(${add180Degree(180)}deg)`;
+        navbarDropdown.classList.remove("display__visible");
+        navbarDropdown.classList.add("hide__visible");
+        arrowDropdown.style.transform = "rotate(0deg)";
     }
-});
-
-window.addEventListener("resize", () => {
-    let widthOutput = window.innerWidth;
-
-    if (widthOutput < 992) {
-        navbarDropdown.classList.remove("display__dropdown");
-        navbarDropdown.classList.remove("hide__dropdown");
-        navbarDropdown.classList.add("hide__base__dropdown");
-        arrowDropdown.style.transform = `rotate(${add180Degree(180)}deg)`;
-    }    
 });
 
 // HANDLE BURGER MENU
 
 burgerNav.addEventListener("click", () => {
-    mobileMenu.classList.add("display__mobile__menu");
-    mobileMenu.classList.remove("hide__mobile__menu");
+    mobileMenu.classList.add("display__transformY");
+    mobileMenu.classList.remove("hide__transformY");
+    burgerNav.classList.add("hide__visible");
+    burgerNav.classList.remove("display__visible");
 });
 
 closerNav.addEventListener("click", () => {
-    mobileMenu.classList.add("hide__mobile__menu");
-    mobileMenu.classList.remove("display__mobile__menu");
+    mobileMenu.classList.add("hide__transformY");
+    mobileMenu.classList.remove("display__transformY");
+    burgerNav.classList.add("display__visible");
+    burgerNav.classList.remove("hide__visible");
+});
+
+// REBOOT ANIMATIONS ON WINDOW RESIZE
+
+window.addEventListener("resize", () => {
+    let widthOutput = window.innerWidth;
+
+    if (widthOutput < 992) {
+        navbarDropdown.classList.remove("display__visible");
+        navbarDropdown.classList.remove("hide__visible");
+        navbarDropdown.classList.add("hide__base");
+        arrowDropdown.style.transform = "rotate(0deg)";
+    } else if (widthOutput > 992) {
+        mobileMenu.classList.remove("display__transformY");
+        mobileMenu.classList.remove("hide__transformY");
+        burgerNav.classList.remove("display__visible");
+        burgerNav.classList.remove("hide__visible");
+    }
 });
