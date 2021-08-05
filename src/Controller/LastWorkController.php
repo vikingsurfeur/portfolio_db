50296acaf1de971d3e5de6e193bd8d747a78d403
David Bouscarle <d.bouscarle@gmail.com>
2021-08-05 16:04:35 +0200

    Add some style to scss pages

--- src/Controller/LastWorkController.php
+++ src/Controller/LastWorkController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PhotographRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class LastWorkController extends AbstractController
 {
     #[Route('/last-work', name: 'last_work')]
-    public function index(): Response
+    public function index(PhotographRepository $photographRepository): Response
     {
         return $this->render('last/index.html.twig', [
             'controller_name' => 'LastWorkController',
+            'photographs' => $photographRepository->lastThreeWork(),
         ]);
     }
 }
